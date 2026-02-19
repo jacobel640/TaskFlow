5bfff46d4c8de719134d9c13fe2ee650cce346ca
jacobel640 <jacobel640@gmail.com>
2026-02-19 17:11:07 +0200

    optimized imports

--- app/src/main/java/com/jel/taskflow/tasks/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/jel/taskflow/tasks/presentation/home/HomeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
